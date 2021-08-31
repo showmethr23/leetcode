13f5ae3daa5446f7a291ecd3e703e590cee28b63
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2021-08-30 20:48:49 -0400

    add 1.py solution 2,3

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,9 +1,13 @@
-#Solution 1
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Solution 1
+        # creating a list to append
         result = []
 
+        # using enumerate to make nums having their indexes
         for index, number in enumerate(nums):
+            # second number to match with the target
             difference = target - number
 
             if difference in nums[index+1]:
@@ -13,4 +17,21 @@ class Solution:
         return result
 
 
+        # Solution 2
+        
+        for num1 in range(0, len(nums)):
+            for num2 in range(num1+1, len(nums)):
+                if nums[num1] + nums[num2] == target:
+                    return [num1, num2]
 
+
+
+        # Solution 3
+
+        visited = {}
+        for index number in enumerate(nums):
+            difference = target - number
+            if difference in visited:
+                return [visited[difference], index]
+            else:
+                visited[number] = index
