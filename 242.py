9cd6339d37c01a23b6b3b9fa238086987b2810be
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2022-07-19 15:59:28 -0400

    Updated

--- 242.py
+++ 242.py
@@ -3,4 +3,28 @@ class Solution:
 
         # Solution 1
 
-        
+        if len(s) != len(t) or not s or not t:
+            return False
+
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False
+
+
+        # Solution 2
+
+        countS, countT = {}, {}
+
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for c in countS:
+            if countS[c] != countT.get(t[i], 0):
+                return False
+
+        return True
