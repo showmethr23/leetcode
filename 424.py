c2b294345d4bcbff1db30796c61be254a574fe4b
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2022-08-05 15:06:01 -0400

    Updated

---
+++ 424.py
@@ -0,0 +1,40 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        # Solution 1
+        # O(26 * n)
+
+        count = {}
+        res = 0
+
+        l = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+
+            res = max(res, r - l + 1)
+        return res
+
+
+        # Solution 2
+        # O(n)
+
+        count = {}
+        res = 0
+
+        l = 0
+        maxf = 0
+        
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            maxf = max(maxf, count[s[r]])
+
+            if (r - l + 1) - maxf > k:
+                count[s[l]] -= 1
+                l += 1
+
+            res = max(res, r - l + 1)
+        return res
