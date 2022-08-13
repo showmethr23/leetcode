aaf7e469c0f6f859103d0d7dd3353ff02de549d0
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2022-08-13 10:28:18 -0400

    Updated

--- 153.py
+++ 153.py
@@ -4,4 +4,20 @@ class Solution:
         # Solution 1
         # Binary search
 
+        l, r = 0, len(nums) -1
+        res = nums[0]
+        
+        while l < r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
 
+            mid = (l+r) // 2
+            res = min(res, nums[mid])
+
+            if nums[mid] >= nums[l]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
+        
