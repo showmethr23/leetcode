9ba37032498c5b9ad864942e6344bc895c46f26b
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2022-08-16 14:07:40 -0400

    Updated

--- 206.py
+++ 206.py
@@ -1,6 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#   def __init__(self, val=0, next=None):
+#       self.val = val
+#       self.next = next
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
         # Solution 1
+        # iteratively
 
+        prev, curr = None, head
 
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+
+        
+        # Solution 2
+        # recursively
+
+        if not head:
+            return None
+
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+
+        return new Head
+
+        
