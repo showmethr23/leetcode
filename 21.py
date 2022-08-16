93f2fd9001a56215724a62ca6ee5d41cf34bc3fe
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2022-08-16 15:20:31 -0400

    Updated

--- 21.py
+++ 21.py
@@ -5,6 +5,9 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        # Solution 1
+        # iteratively
+
         dummy = current = ListNode()
         
         while l1 and l2: # while list1 and list2 are not None
@@ -37,3 +40,24 @@ class Solution:
             list2.next = self.mergeTwoLists(list1, list2.next)
             return list2
 
+        # Solution 3
+        # iteratively
+
+        dummy = ListNode()
+        tail = dummy
+
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+
+        return dummy.next
