f0bc7e7b0843c142562d0aca486dfbd22208bd6a
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2022-08-16 14:51:42 -0400

    Updated

--- 21.py
+++ 21.py
@@ -24,5 +24,16 @@ class Solution:
         return dummy.next
 
         # Solution 2
+        # recursive call
 
+        if not list1:
+            return list2
+        elif not list2:
+            return list1
+        elif list1.val < list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
 
