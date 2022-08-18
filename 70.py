bac77ed30c2f5263af72cd5c9b9b7995d7142e00
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2022-08-17 21:42:34 -0400

    Updated

--- 70.py
+++ 70.py
@@ -4,4 +4,14 @@ class Solution:
         # Solution 1
         # DP
 
+        if n == 1:
+            return 1
 
+        one, two = 1, 1
+
+        for i in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
+
+        return one
