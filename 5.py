02591f5e7682915b29c2d6b3a2e274fa03ac888e
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2022-08-20 11:22:53 -0400

    Updated

--- 5.py
+++ 5.py
@@ -20,3 +20,48 @@ class Solution:
         for i in range(len(s) -1):
             result = max(result, expand(i, i+1), expand(i, i+2), key=len)
         return result
+
+        # Solution 2
+        # DP, Expanding from middle
+
+        if len(s) == 1:
+            return s
+        elif not s:
+            return 0
+        elif len(s) == 2 and s[-1] == s[0]:
+            return s
+
+        res = ""
+        resLen = 0
+
+        # we will see every position in the string
+        for i in range(len(s)):
+
+            # odd length
+
+            l, r = i, i
+            # Iterate until if two pointers are inbound within the string and are palindrome
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                
+                # if they are palindome and longer than previous updated answer
+                if (r - l + 1) > resLen:
+
+                    # Update a answer and answer length
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+
+                # expand the pointers left and right
+                l -= 1
+                r += 1
+
+            # even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                
+                if (r - l + 1) > resLen:
+                    res = s[l:r+1]
+                    resLen = r - l + 1
+
+                l -= 1
+                r += 1
+        return res
