ff4dd91459dead5c12caca7ce94994f3d690a34d
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2022-08-25 13:25:34 -0400

    Updated

--- 102.py
+++ 102.py
@@ -9,6 +9,25 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
 
         # Solution 1
-        # Recursion
+        # BFS
 
+        res = []
+
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+
+            temp = []
+
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    temp.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if temp:
+                res.append(temp)
+        return res
 
