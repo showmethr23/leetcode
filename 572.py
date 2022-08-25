c8251250b7c349a61431476bf47f13073e3aa799
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2022-08-25 12:29:44 -0400

    Updated

--- 572.py
+++ 572.py
@@ -5,11 +5,26 @@
 #       self.left = left
 #       self.right = right
 
-from typing import Optional
-
 
 class Solution:
     def isSubtree(self, root:Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
 
         # Solution 1
+        # Recursion
         
+        if not subRoot:
+            return True
+        if not root:
+            return False
+
+        if sameTree(root, subRoot):
+            return True
+        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+
+    def sameTree(self, root, subRoot):
+        if not root and subRoot:
+            return True
+
+        if root and subRoot and root.val == subRoot.val:
+            return (self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right))
+        return False
