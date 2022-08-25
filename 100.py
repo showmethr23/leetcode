8adaeed09e9cedfef8b4275e9210d2c180474837
showmethr23 <62336688+showmethr23@users.noreply.github.com>
2022-08-25 11:51:23 -0400

    Updated

--- 100.py
+++ 100.py
@@ -11,4 +11,15 @@ class Solution:
         # Solution 1
         # Recursion
 
+        # Base case
+        if not p and not q:
+            return True
+
+        if not p or not q:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
