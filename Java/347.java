1c6ba257ba30f6cd6b918099f1ed6e2101d3fcf0
showmethr23 <showmethr23@hotmail.com>
2023-02-13 13:11:06 -0500

    347.java updated

---
+++ Java/347.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k){
+
+        // freq map
+        Map<Integer, Integer> freq = new HashMap<Integer, Integer>();
+        for (int n : nums) {
+            freq.put(n, freq.getOrDefault(n, 0) + 1);
+        }
+        // bucket sort on freq
+        List<Integer>[] bucket = new List[nums.length + 1];
+        for (int i = 0; i < bucket.length; i++) bucket[i] = new ArrayList();
+        for (int key : freq.keySet()) {
+            bucket[freq.get(key)].add(key);
+        }
+        // gather result
+        List<Integer> res = new ArrayList();
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            res.addAll(bucket[i]);
+            if (res.size() >= k) break;
+        }
+        return res;
+
+    }
+}
