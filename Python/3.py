ab5a27522ba069b7185a2913461fae2efb262b44
showmethr23 <showmethr23@hotmail.com>
2023-03-14 11:04:07 -0400

    3.py updated

--- Python/3.py
+++ Python/3.py
@@ -4,3 +4,23 @@ class Solution:
         # Solution 1
         # sliding windoe
         
+        n = len(s)
+        res = 0
+        charset = set()
+        l = 0
+        # base case
+        if n == 0:
+            return 0
+
+        if n == 1:
+            return 1
+        
+        for r in range(n):
+            while s[r] in charset:
+                charset.remove(s[l])
+                l += 1
+            
+            charset.add(s[r])
+            res = max(res, r - l +1)
+        
+        return res
