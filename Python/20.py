38d03250eeaf4da96914df04585bab4ed9c13751
showmethr23 <showmethr23@hotmail.com>
2023-03-20 13:28:36 -0400

    20.py updated

--- Python/20.py
+++ Python/20.py
@@ -36,4 +36,16 @@ class Solution:
             return False
         return True 
 
+        # Solution 3
+
+        if len(s) == 0:
+            return True
+        opcl = dict(('()', '[]', '{}'))
+        stack = []
+        for idx in s:
+            if idx in '([{':
+                stack.append(idx)
+            elif len(stack) == 0 or idx != opcl[stack.pop()]:
+                return False
+        return len(stack) == 0
 
