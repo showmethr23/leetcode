54e9ded2ef22274b053cb7926018d9ac253ec17f
Junyeong <showmethr23@hotmail.com>
2023-08-13 15:47:16 -0400

    392.py updated

--- Python/392.py
+++ Python/392.py
@@ -4,6 +4,13 @@ class Solution:
         # Solution 1
         # using two-pointers
 
+        # base case
+        if len(s) < 1:
+            return True
+        
+        if len(s) > len(t):
+            return false
+
         i, j = 0, 0
 
         while i < len(s) and j < len(t):
@@ -15,3 +22,25 @@ class Solution:
             return True
         else:
             False
+
+        # Solution 2
+        # using counts
+
+        # base case
+        if len(s) < 1:
+            return True;
+
+        if len(s) > len(t):
+            return False;
+
+        subs = 0
+
+        for i in range(len(t)):
+            if subs <= len(s):
+                if s[subs] == t[i]:
+                    subs += 1
+
+        if len(s) == subs:
+            return True;
+
+        return False;
