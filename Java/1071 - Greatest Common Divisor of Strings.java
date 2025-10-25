487ba8d3dff746ef8fcfbb4d6e8c9b5d38c1b69e
Junyeong Choi <showmethr23@hotmail.com>
2025-10-25 15:39:48 -0400

    Added 1071 Solution

--- Java/1071 - Greatest Common Divisor of Strings.java
+++ Java/1071 - Greatest Common Divisor of Strings.java
@@ -53,3 +53,24 @@ Constraints:
 */
 
 // Solution 1 - 
+class Solution {
+    private int gcd(int a, int b){
+        if (b == 0){
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2+str1)){
+            return "";
+        }
+        
+        int len1 = str1.length();
+        int len2 = str2.length();
+
+        int GCD = gcd(len1, len2);
+
+        return str1.substring(0, GCD);
+    }
+}
